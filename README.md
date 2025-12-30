c521c683436c9486bca4e442189316c468c3c587
Diego Echeverría <diego.pucalan@gmail.com>
2025-12-30 18:53:20 -0300

    Forks Test

---
+++ README.md
@@ -0,0 +1,17 @@
+My-Own-Shell.
+# Why make your own shell?
+### Still in the making. We'll see later
+
+
+# What is a Shell?
+### Ok, so in simple terms its the interface from which we comunate to the operating system (OS). Be it by using comands,
+### files, etc.
+
+### The purpose of it, is to translate our needs to the Kernel. 
+
+DISCLAIMER:
+1. This project was entirely made by hand. No AI was used to write or ask for solutions. All in order to learn everything
+needed to become a better programmer.
+2. My native language is Spanish, but this is programmed in English with no AI all made by myself. 
+
+
--- main.c
+++ main.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
-#include <unistd.h> # UNIX libraries
-#include <sys/wait.h> # Wait forks use this
-#include <sys/types.h> # Types of data
-#include <stdlib.h> # Memory Management
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+// First Introduction to Forks
 
 int main(){
 	
+	int pid = fork();
+	
+	if (pid == 0){
+		printf("this is the child process with this parent id: %d and this child id: %d",getpid(),pid);
+		}
+	else if (pid!=0){
+		printf("this is the parent process with this parent id: %d",getpid());
+		}
 	while(1){
 		char buffer[100]; //Buffer of the input maximum of 99 characters plus null terminator
 		printf("Input$: ");
